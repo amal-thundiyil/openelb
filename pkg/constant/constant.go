cb378138a176e083f96632e0ef5821c3d48bab52
Amal Thundiyil <amal.s.thundiyil@gmail.com>
2022-08-05 09:36:24 +0530

    add cm name
    Signed-off-by: Amal Thundiyil <amal.s.thundiyil@gmail.com>

--- pkg/constant/constant.go
+++ pkg/constant/constant.go
@@ -46,7 +46,7 @@ const (
 	OpenELBSpeakerLocker      = "openelb-speaker"
 	OpenELBNamespace          = "openelb-system"
 	OpenELBVipConfigMap       = "openelb-vip-configmap"
-	OpenELBBgpConfigMap       = "openelb-bgp-configmap"
+	OpenELBBgpConfigMap       = "clusters-config"
 	OpenELBBgpName            = "openelb-bgp"
 	OpenELBVipName            = "openelb-keepalive-vip"
 	OpenELBServiceAccountName = "kube-keepalived-vip"
